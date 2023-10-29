34ca40024e65334a228530509046037baea7af63
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-10-29 17:15:40 +0400

    Update

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            String word = sourceList.get(i);
+            result.add(word);
+        }
+
+        return result;
     }
 }
+
--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                result.addLast(num); // Insert even numbers at the end
+            } else {
+                result.addFirst(num); // Insert odd numbers at the beginning
+            }
+        }
+
+        return result;
     }
 }
+
